a51c8508e82573d15306c06ab3adf72bd0477275
Jennie Vu <jennievu8991@gmail.com>
2022-09-23 15:41:29 -0400

    Reimplement home view, added register button

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,24 +1,32 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import ListSalon from "../components/list-salon/list-salon";
+import Navbar from "../components/navbar/navbar";
+import Searchbar from "../components/search-bar/search-bar";
+import { Link } from 'react-router-dom';
+
 
 class Home extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = { apiResponse: ""};
+    this.state = { apiResponse: "" };
   }
-
-  render(){
-    return(
+  
+  render() {
+    const title = 'Start Your Business';
+    return (
       <div>
-
-        <h1> Home page </h1>
-
+        <Navbar></Navbar>
+        <div>
+          <Searchbar></Searchbar>
+          <ListSalon></ListSalon>
+        </div>
+        <div>
+          {title}
+          <Link to="/sign-up"><button type="button">Register your business now!</button></Link>
+        </div>
       </div>
     );
-
   }
-
-
 }
 
 export default Home;
