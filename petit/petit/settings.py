45a448e0aa7dd85f99b388de0474f1bbbea2bc3f
jisoopark <jisoopark3@gmail.com>
2022-09-22 20:26:05 -0400

    Configured settings.py to be able to connect to mysql

--- petit/petit/settings.py
+++ petit/petit/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'petit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': BASE_DIR / 'db.mysql',
+        'USER': 'petit',
+        'PASSWORD': 'petit123',
+        'HOST': '127.0.0.1',
+        'PORT': '8000',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
