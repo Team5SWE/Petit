f8236935e2095b9f8d789e454230a2a507925639
GuillermoClara <guillexclara@gmail.com>
2022-09-23 18:48:22 -0400

    Created login page

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,11 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css';
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
-import SignUp from "./pages/SignUp";
 import ContactUs from "./pages/ContactUs";
+
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+
 
 class App extends Component {
 
@@ -14,7 +17,10 @@ class App extends Component {
         <Routes>
           <Route path="/">
             <Route index element={<Home/>} />
+
             <Route path="sign-up" element={<SignUp/>} />
+            <Route path="login" element={<Login/>} />
+
             <Route path="contact" element={<ContactUs/>} />
             <Route path="*" element={<NotFound/>} />
           </Route>
