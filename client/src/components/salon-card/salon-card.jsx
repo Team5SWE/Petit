325cadefd6e264b815757b6b910234013a90939a
Jennie Vu <jennievu8991@gmail.com>
2022-09-22 20:16:33 -0400

    Created new components for homepage

---
+++ client/src/components/salon-card/salon-card.jsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+export class SalonCard extends Component {
+    render() {
+        return <div>
+            Salon {this.props.name}
+        </div>
+    }
+}
---
+++ client/src/components/search-bar/search-bar.jsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+export default class Searchbar extends Component {
+    render() {
+        return <div>
+            Search bar here
+        </div>
+    }
+}
