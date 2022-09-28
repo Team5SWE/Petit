9c6f15238019e104a13bef4be6a460f059ea18b8
GuillermoClara <guillexclara@gmail.com>
2022-09-28 13:11:14 -0400

    Addeded business view

--- petit/base/views.py
+++ petit/base/views.py
@@ -3,11 +3,13 @@ from django.http import HttpResponse
 import json
 import sys
 from .models import Business, Appointment, Employee, Address
-from utility import date_manager
+
+from petit.utility import date_manager, csv
 
 # Create your views here.
 def index(response):
     return HttpResponse("This is a test of our first view")
+
 
 def view1(response, id):
     ls = Business.objects.get(id=id)
@@ -15,10 +17,17 @@ def view1(response, id):
     return HttpResponse(json.dumps(response_data), content_type="application/json")
 
 
-def appointments(response, appointmentId):
+def get_appointment(appointment_id):
+    """
+    View that handles API get request from an appointment
 
-    appointment = Appointment.objects.get(id=appointmentId)
+    Returns an HTTP Response in JSON format with all data
+    from database related to the appointment
+    """
 
+    appointment = Appointment.objects.get(id=appointment_id)
+
+    # Create dictionary to be sent as JSON
     response_data = dict()
     response_data['valid'] = False
 
@@ -30,6 +39,7 @@ def appointments(response, appointmentId):
 
     business = Business.objects.get(id=appointment.business_id)
 
+    # Placeholders in case data is missing
     employee_name = "TBA"
 
     address_name = "TBA"
@@ -49,7 +59,7 @@ def appointments(response, appointmentId):
     end = appointment.end
     service = appointment.service
 
-    if date_manager.has_expired(date, end) :
+    if date_manager.has_expired(date, end):
         response_data['finished'] = True
     else:
         response_data['finished'] = False
@@ -64,6 +74,24 @@ def appointments(response, appointmentId):
     response_data['address'] = address_name
 
     return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+
+def get_business(business_id):
+
+    business = Business.objects.get(id=business_id)
+
+    response_data = dict()
+    response_data['valid'] = False
+
+    if business is None:
+        return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+    response_data['name'] = business.name
+    response_data['email'] = business.email
+    response_data['description'] = business.description
+    response_data['services'] = csv.csv_to_list(business.services)
+
+    
 
 
 
