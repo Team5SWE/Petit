4ee67736f1bb5c0e237b3e996972da801a76d235
Jennie Vu <jennievu8991@gmail.com>
2022-10-22 19:30:26 -0400

    Added mock data for visualization

--- client/src/components/calendar/calendar.jsx
+++ client/src/components/calendar/calendar.jsx
@@ -50,7 +50,43 @@ class Calendar extends Component {
   get calendar() {
     return this.calendarRef.current.control;
   }
+  componentDidMount() {
 
+    const events = [
+      {
+        id: 1,
+        text: "Event 1",
+        start: "2023-03-07T10:30:00",
+        end: "2023-03-07T13:00:00"
+      },
+      {
+        id: 2,
+        text: "Event 2",
+        start: "2023-03-08T09:30:00",
+        end: "2023-03-08T11:30:00",
+        backColor: "#6aa84f"
+      },
+      {
+        id: 3,
+        text: "Event 3",
+        start: "2023-03-08T12:00:00",
+        end: "2023-03-08T15:00:00",
+        backColor: "#f1c232"
+      },
+      {
+        id: 4,
+        text: "Event 4",
+        start: "2023-03-06T11:30:00",
+        end: "2023-03-06T14:30:00",
+        backColor: "#cc4125"
+      },
+    ];
+
+    const startDate = "2023-03-07";
+
+    this.calendar.update({startDate, events});
+
+  }
   render() {
     return (
       <div style={styles.wrap}>
