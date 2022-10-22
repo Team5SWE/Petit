def0962027be59fa51613f833db764b012163e04
Jennie Vu <jennievu8991@gmail.com>
2022-10-22 19:35:42 -0400

    Added calendar component into View All Appointments

--- client/src/pages/Schedule.jsx
+++ client/src/pages/Schedule.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Calendar from '../components/calendar/calendar'
 
 function Schedule() {
   return (
     <div>
-      <h1>View all  Appointments</h1>  
+      <h1>View all  Appointments</h1>
+      <Calendar />
     </div>
   );
 }
