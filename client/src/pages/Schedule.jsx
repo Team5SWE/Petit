c84ad62f65f9667e917591ad6eccd18cdac661c7
aleeza219 <aleeza219@yahoo.com>
2022-10-22 17:01:39 -0400

    created settings page

--- client/src/pages/Schedule.jsx
+++ client/src/pages/Schedule.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function Employees() {
+function Appointments() {
   return (
     <div>
       <h1>View all  Appointments</h1>  
     </div>
   );
 }
-export default Employees;
+export default Appointments;
