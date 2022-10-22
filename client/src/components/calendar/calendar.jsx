4e0cb8e15d0867505596c3a3fa3ba2b27fb9da2c
Jennie Vu <jennievu8991@gmail.com>
2022-10-22 19:26:44 -0400

    Implement weekly calendar

--- client/src/components/calendar/calendar.jsx
+++ client/src/components/calendar/calendar.jsx
@@ -1,13 +1,60 @@
 import React, {Component} from 'react';
-import {DayPilot, DayPilotNavigator} from "@daypilot/daypilot-lite-react";
+import {DayPilot, DayPilotCalendar, DayPilotNavigator} from "@daypilot/daypilot-lite-react";
 import "./calendar.css";
+
+const styles = {
+  wrap: {
+    display: "flex"
+  },
+  left: {
+    marginRight: "10px"
+  },
+  main: {
+    flexGrow: "1"
+  }
+};
 
 class Calendar extends Component {
 
+  constructor(props) {
+    super(props);
+    this.calendarRef = React.createRef();
+    this.state = {
+      viewType: "Week",
+      durationBarVisible: false,
+      timeRangeSelectedHandling: "Enabled",
+      onTimeRangeSelected: async args => {
+        const dp = this.calendar;
+        const modal = await DayPilot.Modal.prompt("Create a new event:", "Event 1");
+        dp.clearSelection();
+        if (!modal.result) { return; }
+        dp.events.add({
+          start: args.start,
+          end: args.end,
+          id: DayPilot.guid(),
+          text: modal.result
+        });
+      },
+      eventDeleteHandling: "Update",
+      onEventClick: async args => {
+        const dp = this.calendar;
+        const modal = await DayPilot.Modal.prompt("Update event text:", args.e.text());
+        if (!modal.result) { return; }
+        const e = args.e;
+        e.data.text = modal.result;
+        dp.events.update(e);
+      },
+    };
+  }
+
+  get calendar() {
+    return this.calendarRef.current.control;
+  }
+
   render() {
     return (
-      <div>
-        <div>
+      <div style={styles.wrap}>
+        <div style={styles.left}>
           <DayPilotNavigator
             selectMode={"week"}
             showMonths={3}
@@ -19,6 +66,12 @@ class Calendar extends Component {
                 startDate: args.day
               });
             }}
+          />
+        </div>
+        <div style={styles.main}>
+          <DayPilotCalendar
+            {...this.state}
+            ref={this.calendarRef}
           />
         </div>
       </div>
